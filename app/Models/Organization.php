86da779b2f886c076f767d212ba1e7e23159a120
MuhammadAbdulMoiz <moiza354@gmail.com>
2023-12-22 10:10:07 +0500

    migration file changes

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -14,6 +14,7 @@ class Organization extends Model
 
     protected $fillable = [
         'owner_id',
+        'name',
         // Additional Fields
         'members',
         'teams',
--- database/migrations/2023_12_20_161517_create_organizations_table.php
+++ database/migrations/2023_12_20_161517_create_organizations_table.php
@@ -10,6 +10,7 @@ class CreateOrganizationsTable extends Migration
     {
         Schema::create('organizations', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
             $table->foreignId('owner_id')->constrained('users');
             // Additional Fields
             $table->json('members')->nullable(); // Assuming members are stored as JSON array
