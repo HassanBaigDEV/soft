1fddc8107fe17fefcf10d859e98b9ee73d1662d5
HassanBaigDEV <hassanbaig923@gmail.com>
2023-12-24 01:31:07 +0500

    minor content changes in register

--- resources/views/session/register.blade.php
+++ resources/views/session/register.blade.php
@@ -9,7 +9,7 @@
         <div class="row justify-content-center">
           <div class="col-lg-5 text-center mx-auto">
             <h1 class="text-white mb-2 mt-5">Welcome!</h1>
-            <p class="text-lead text-white">Use these awesome forms to login or create new account in your project for free.</p>
+            <p class="text-lead text-white">Fill in this form to create a new account for free.</p>
           </div>
         </div>
       </div>
